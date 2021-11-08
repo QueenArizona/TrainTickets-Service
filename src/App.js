3c4090c59053fb59c1d71458c180f909c602f90d
QueenArizona <queen483arizona@gmail.com>
2021-11-08 13:38:16 +0300

    add: homepage Creating Homepage with route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/main/HomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <main className="main">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/main/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "../home/About";
+import Faq from "../home/Faq";
+import Reviews from "../home/Reviews";
+
+function HomePage(props) {
+  return (
+    <>
+      <About />
+      <Faq />
+      <Reviews />
+    </>
+  );
+}
+
+export default HomePage;
