2e0f060ac5666d581bdaeeaaa79d72f0f024b43f
QueenArizona <queen483arizona@gmail.com>
2021-11-08 13:49:28 +0300

    add: header Creating header with navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/Header";
 import HomePage from "./components/main/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <main className="main">
           <Switch>
             <Route exact path="/" component={HomePage} />
